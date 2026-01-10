ebcb0ba97796a8f46bcf9531e3637cfba2dc6456
José Márcio <jjmarcio159@gmail.com>
2026-01-10 11:18:03 -0300

    refactor(app): ajustes em App.tsx e navegação principal

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
-import 'react-native-gesture-handler'
-import React from 'react'
-import { StatusBar } from 'react-native'
-import AppNavigator from './src/navigation'
-import { AuthProvider } from './src/context/AuthContext'
+import React from "react";
+import AppNavigator from "./src/navigation/index"; // ou o caminho do seu AppNavigator
+import { Provider as PaperProvider } from "react-native-paper";
+import { AuthProvider } from "./src/context/AuthContext";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-      <AppNavigator />
-    </AuthProvider>
+    <PaperProvider>
+      <AuthProvider>
+        <AppNavigator />
+      </AuthProvider>
+    </PaperProvider>
   );
 }
+
