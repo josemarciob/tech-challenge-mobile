71ee6b3209913df27df21e499b64dbebc840d3fc
Jose Marcio <jose.souza.24716@a.fecaf.com.br>
2026-01-10 11:18:03 -0300

    refactor(app): ajustes em App.tsx e navegação principal

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,12 +1,19 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { enableScreens } from 'react-native-screens'
+import { NavigationContainer } from "@react-navigation/native"; 
+import { createNativeStackNavigator} from "@react-navigation/native-stack";
+import { enableScreens } from "react-native-screens";
 
-import LoginScreen from '../screens/Login'
-import PostsList from '../screens/PostsList'
-import PostDetail from '../screens/PostDetail'
-import CreatePost from '../screens/CreatePost'
-enableScreens(); 
+import LoginScreen from "../screens/Login";
+import PostsList from "../screens/PostsList";
+import PostDetail from "../screens/PostDetail";
+import CreatePost from "../screens/CreatePost";
+import EditPostScreen from "../screens/EditPostScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+import HomeScreen from "../screens/HomeScreen";
+import Perfil from "../screens/Perfil";
+import AdminScreen from "../screens/AdminScreen";
+import UserDetail from "../screens/UserDetail";
+
+enableScreens();
 
 const Stack = createNativeStackNavigator();
 
@@ -19,9 +26,15 @@ export default function AppNavigator() {
           component={LoginScreen}
           options={{ headerShown: false }}
         />
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Atividades" component={PostsList} />
-        <Stack.Screen name="DetalhesAtividade" component={PostDetail} />
-        <Stack.Screen name="CriarAtividade" component={CreatePost} />
+        <Stack.Screen name="Detalhes Atividade" component={PostDetail} />
+        <Stack.Screen name="Criar Atividade" component={CreatePost} />
+        <Stack.Screen name="Edite Atividade" component={EditPostScreen} />
+        <Stack.Screen name="Cadastro" component={RegisterScreen} />
+        <Stack.Screen name="Perfil" component={Perfil} />
+        <Stack.Screen name="AdminScreen" component={AdminScreen} />
+        <Stack.Screen name="UserDetail" component={UserDetail} />
       </Stack.Navigator>
     </NavigationContainer>
   );
